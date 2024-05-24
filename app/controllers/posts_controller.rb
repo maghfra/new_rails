e9e7afcbb5a56e44aff98f06acbe031b72e8702b
maghfra <maghferaelorbany6@gmail.com>
2024-05-24 22:53:25 +0300

    Add CRUD operations for Post resource

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,45 @@
 class PostsController < ApplicationController
-    def index
 
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new({title: params[:post][:title], content: params[:post][:content]})
+
+    if @post.save
+      redirect_to post_url(@post), notice: "Post was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
-  
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update({title: params[:post][:title], content: params[:post][:content]})
+      redirect_to post_url(@post), notice: "Post was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_url, notice: "Post was successfully destroyed."
+  end
+
+end 
