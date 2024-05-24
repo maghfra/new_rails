1af6b7584db73980b59d13d20bf4e78c4d053440
maghfra <maghferaelorbany6@gmail.com>
2024-05-24 23:41:50 +0300

    Add address column to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_24_200337) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_203456) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_24_200337) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
